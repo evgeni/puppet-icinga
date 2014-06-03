6d77bf00b6632cf44caf7b8ff8d6ae79f4db7167
Evgeni Golov <evgeni@golov.de>
2014-06-03 21:37:58 +0200

    y I no can haz mocha, travis?

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,13 +2,13 @@ dir = File.expand_path(File.dirname(__FILE__))
 $LOAD_PATH.unshift File.join(dir, 'lib')
 fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
 
-require 'mocha'
+#require 'mocha'
 require 'puppet'
 require 'rspec-puppet'
 require 'rspec/autorun'
 
 RSpec.configure do |config|
-    config.mock_with :mocha
+#    config.mock_with :mocha
     config.module_path = File.join(fixture_path, 'modules')
     config.manifest_dir = File.join(fixture_path, 'manifests')
 end
