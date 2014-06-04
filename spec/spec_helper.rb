7d50f8cf542aa450d78c98128661516216c7dd23
Evgeni Golov <evgeni@golov.de>
2014-06-04 17:17:05 +0200

    simplify spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,4 @@
 dir = File.expand_path(File.dirname(__FILE__))
 $LOAD_PATH.unshift File.join(dir, 'lib')
-fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
 
-#require 'mocha'
-require 'puppet'
-require 'rspec-puppet'
-
-RSpec.configure do |config|
-#    config.mock_with :mocha
-    config.module_path = File.join(fixture_path, 'modules')
-    config.manifest_dir = File.join(fixture_path, 'manifests')
-end
+require 'puppetlabs_spec_helper/module_spec_helper'
